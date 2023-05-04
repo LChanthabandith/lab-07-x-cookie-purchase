180ff4467e57a97f9b75220aaf5b17b7d0fa1d2d
Lavieng Chanthabandith <LChanthabandith@gmail.com>
2023-05-04 03:52:12 -0500

    adding files to GitHub lab07X

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# lab-07-x-cookie-purchase
+# LAB - 07 - X
+
+## Cookie Purchase
+
+### Author: Lavieng Chanthabandith
+
+### Links and Resources
+
+[How to Style a Modern Website](https://www.youtube.com/watch?v=Sv_NAxi_jNs)
+
+[How to link 2 HTML files together](https://www.youtube.com/watch?v=Ogm7qZRkm1I)
+
+[JavaScript if else](https://www.youtube.com/watch?v=IsG4Xd6LlsM)
+
+[JavaScript Number toFixed()](https://www.w3schools.com/jsref/jsref_tofixed.asp)
+
+[Basic JavaScript #18: Math operators](https://www.ySoutube.com/watch?v=SpUJIE0hcJg)
+
+[HTML DOM Document write()](https://www.w3schools.com/jsref/met_doc_write.asp)
+
+[Javascript method document.write() explained](https://www.youtube.com/watch?v=MY2X203FtRI)
+
+
+### Lighthouse Accessibility Report Score
+
+[Lighthouse Report](lab07X-LHS.png)
---
+++ macadamia-nut-cookie.js
@@ -0,0 +1,44 @@
+// input for the amount of customer cookie
+var numCookies = prompt("How many cookies do you want to purchase?");
+
+// input for discount code
+var discountCode = prompt("Do you have a discount code? Enter '10OFF' for 10% off or '5OFF' for $5 off.");
+
+// set prices for product, tax, and shipping
+var pricePerCookie = 1.75;
+var taxRate = 0.052;
+var shippingCost = 3.99;
+
+// calculate subtotal
+var subtotal = numCookies * pricePerCookie;
+
+// apply discount code
+var discount = 0.0;
+if (discountCode === "10OFF") {
+  discount = subtotal * 0.1;
+} else if (discountCode === "5OFF") {
+  discount = 5.0;
+}
+
+// calculate total before tax and shipping
+var totalBeforeTaxAndShipping = subtotal - discount;
+
+// calculate tax
+var tax = totalBeforeTaxAndShipping * taxRate;
+
+// calculate total after tax
+var totalAfterTax = totalBeforeTaxAndShipping + tax;
+
+// calculate total after tax and shipping
+var total = totalAfterTax;
+if (totalAfterTax < 25.0) {
+  total += shippingCost;
+}
+
+// write results to the page
+document.write("<p>Number of cookies: " + numCookies + "</p>");
+document.write("<p>Price per cookie: $" + pricePerCookie.toFixed(2) + "</p>");
+document.write("<p>Discount: $" + discount.toFixed(2) + "</p>");
+document.write("<p>Tax: $" + tax.toFixed(2) + "</p>");
+document.write("<p>Shipping cost: $" + shippingCost.toFixed(2) + "</p>");
+document.write("<p>Total cost: $" + total.toFixed(2) + "</p>");
